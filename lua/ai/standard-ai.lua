f29858867b010050821e0def9a411ea2420684fc
Paracel <paracel_007@sina.com.cn>
2013-01-30 00:23:20 +0800

    intention of TuXi for non-AI players (by haveatry823)

--- lua/ai/standard-ai.lua
+++ lua/ai/standard-ai.lua
@@ -363,14 +363,23 @@ end
 
 sgs.ai_card_intention.TuxiCard = function(self, card, from, tos)
 	local lord = from:getRoom():getLord()
-	local tuxi_lord = false
-	for _, to in ipairs(tos) do
-		if to:objectName() == lord:objectName() then tuxi_lord = true end
-		local intention = from:hasFlag("tuxi_isfriend_" .. to:objectName()) and -5 or 80
-		sgs.updateIntention(from, to, intention)
-	end
-	if sgs.turncount == 1 and not tuxi_lord and not lord:isKongcheng() and not from:getRoom():alivePlayerCount() == 2 then
-		sgs.updateIntention(from, lord, -80)
+	local tuxi_lord = false  
+	if from:getState() == "online" then
+		for _, to in ipairs(tos) do
+			if (self:hasSkills("kongcheng|zhiji|lianying", to) and to:getHandcardNum() == 1 ) or to:hasSkill("tuntian") then
+			else
+				sgs.updateIntention(from, to, 80)
+			end
+		end
+	else
+		for _, to in ipairs(tos) do
+			if to:objectName() == lord:objectName() then tuxi_lord = true end
+			local intention = from:hasFlag("tuxi_isfriend_" .. to:objectName()) and -5 or 80
+			sgs.updateIntention(from, to, intention)
+		end
+		if sgs.turncount == 1 and not tuxi_lord and not lord:isKongcheng() and not from:getRoom():alivePlayerCount() == 2 then 
+			sgs.updateIntention(from, lord, -80) 
+		end
 	end
 end
 

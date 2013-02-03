25ed6e5d830bb2c386fb25e85576b4b982a55551
Paracel <paracel_007@sina.com.cn>
2013-02-03 22:58:56 +0800

    typo

--- lua/ai/wind-ai.lua
+++ lua/ai/wind-ai.lua
@@ -213,7 +213,7 @@ function sgs.ai_slash_prohibit.leiji(self, to, card)
 				break
 			end
 		end    
-		if not other_rebel and (self:hasSkills("hongyan") or self.player:getHp() >= 4) and (self:getKnownCard("Peach") > 0 or self.player:hasSkills("ganglie|neoganglie")) then
+		if not other_rebel and ((self.player:getHp() >= 4 and (self:getKnownCard("Peach") > 0 or self:hasSkills("ganglie|neoganglie"))) or self:hasSkill("hongyan")) then
 			return false
 		end
 	end

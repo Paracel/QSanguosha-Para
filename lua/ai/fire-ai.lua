d8ee7d296bab3d45e9777ec494555b3f2f2e47ad
Paracel <paracel_007@sina.com.cn>
2013-02-17 02:48:05 +0800

    typo

--- lua/ai/fire-ai.lua
+++ lua/ai/fire-ai.lua
@@ -268,7 +268,7 @@ lianhuan_skill.getTurnUseCard = function(self)
 				self:useTrickCard(acard, dummy_use)
 				if dummy_use.card then shouldUse = false end
 			end
-			if acard:getTypeId() == sgs.sgs.Card_TypeEquip then
+			if acard:getTypeId() == sgs.Card_TypeEquip then
 				local dummy_use = { isDummy = true }
 				self:useEquipCard(acard, dummy_use)
 				if dummy_use.card then shouldUse = false end

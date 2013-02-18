7ebf4946a72528d634c9811334ce7fd886ea6be5
Paracel <paracel_007@sina.com.cn>
2013-02-19 00:49:20 +0800

    one left out when unifying light boxes

--- src/server/gamerule.cpp
+++ src/server/gamerule.cpp
@@ -717,8 +717,7 @@ bool HulaoPassMode::trigger(TriggerEvent event, Room *room, ServerPlayer *player
             ServerPlayer *lord = room->getLord();
             room->setPlayerMark(lord, "secondMode", 1);
             room->changeHero(lord, "shenlvbu2", true, true);
-            room->broadcastInvoke("animate", "lightbox:$StageChangeAnimate:5000");
-            room->getThread()->delay(5000);
+            room->doLightbox("$StageChange", 5000);
 
             QList<const Card *> tricks = lord->getJudgingArea();
             foreach (const Card *trick, tricks) {

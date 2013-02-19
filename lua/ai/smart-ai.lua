3fd4a827afb859aa7fc5e34bd5fda619a2e3ce8c
Paracel <paracel_007@sina.com.cn>
2013-02-19 19:10:19 +0800

    minor adjustment about equip usage with the skill RenJie

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -4210,7 +4210,7 @@ function SmartAI:useEquipCard(card, use)
 	local same = self:getSameEquip(card)
 	if same then
 		if (self:hasSkills("rende|qingnang|nosgongqi"))
-		or (self.player:hasSkill("yongsi") and self:getOverflow() < 3)
+		or (self:hasSkills("yongsi|renjie") and self:getOverflow() < 2)
 		or (self:hasSkills("qixi|duanliang") and (card:isBlack() or same:isBlack()))
 		or (self:hasSkills("guose|longhun") and (card:getSuit() == sgs.Card_Diamond or same:getSuit() == sgs.Card_Diamond))
 		or (self:hasSkill("jijiu") and (card:isRed() or same:isRed())) then return end

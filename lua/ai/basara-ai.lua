e945c8477fd402e99955408e1b7210dc86d97d2a
Paracel <paracel_007@sina.com.cn>
2013-02-20 22:53:55 +0800

    adjust AI in Hegemony mode by haveatry823

--- lua/ai/basara-ai.lua
+++ lua/ai/basara-ai.lua
@@ -1,4 +1,4 @@
-sgs.ai_skill_choice.RevealGeneral = function(self, choices)
+function askForShowGeneral(self, choices)
 	local event = self.player:getTag("event"):toInt()
 	local data = self.player:getTag("event_data")
 	local generals = self.player:getTag("roles"):toString():split("+")
@@ -12,15 +12,31 @@ sgs.ai_skill_choice.RevealGeneral = function(self, choices)
 	if event == sgs.DamageInflicted then
 		local damage = data:toDamage()
 		for _, player in ipairs(players) do
-			if self:hasSkills(sgs.masochism_skill, player) and self:isEnemy(damage.from, damage.to) then return "yes" end
+			if damage and self:hasSkills(sgs.masochism_skill .. "|zhichi|zhiyu|fenyong", player) and not self:isFriend(damage.from, damage.to) then return "yes" end
+			if damage and damage.damage > self.player:getHp() + self:getAllPeachNum() then return "yes" end
 		end
 	elseif event == sgs.CardEffected then
 		local effect = data:toCardEffect()
 		for _, player in ipairs(players) do
 			if self.room:isProhibited(effect.from, player, effect.card) and self:isEnemy(effect.from, effect.to) then return "yes" end
+			if self:hasSkills("xiangle", player) and effect.card:isKindOf("Slash") then return "yes" end
+			if self:hasSkills("jiang", player) and ((effect.card:isKindOf("Slash") and effect.card:isRed()) or effect.card:isKindOf("Duel")) then return "yes" end
+			if self:hasSkills("tuntian", player) then return "yes" end
 		end
 	end
 
+	if self.room:alivePlayerCount() <= 3 then return "yes" end
+	if sgs.getValue(self.player) < 6 then return "no" end
+	local skills_to_show = "bazhen|yizhong|zaiqi|feiying|buqu|kuanggu|guanxing|luoshen|tuxi|zhiheng|qiaobian|longdan|liuli|longhun|shelie|luoying|anxian|yicong|wushuang|jueqing|niepan"
+	for _, player in ipairs(players) do
+		if self:hasSkills(skills_to_show, player) then return "yes" end
+	end
+	if self.player:getDefensiveHorse() and self.player:getArmor() and not self:isWeak() then return "yes" end	
+end
+
+sgs.ai_skill_choice.RevealGeneral = function(self, choices)
+	if askForShowGeneral(self, choices) == "yes" then return "yes" end
+	
 	local anjiang = 0
 	for _, player in sgs.qlist(self.room:getOtherPlayers(self.player)) do
 		if player:getGeneralName() == "anjiang" then
@@ -44,46 +60,12 @@ if sgs.GetConfig("EnableHegemony", false) then
 		end
 		init(self, player)
 	end
-	sgs.ai_skill_choice.RevealGeneral = function(self, choices)
-		local event = self.player:getTag("event"):toInt()
-		local data = self.player:getTag("event_data")
-		local generals = self.player:getTag("roles"):toString():split("+")
-		local players = {}
-		for _, general in ipairs(generals) do
-			local player = sgs.ServerPlayer(self.room)
-			player:setGeneral(sgs.Sanguosha:getGeneral(general))
-			table.insert(players, player)
-		end
+	sgs.ai_skill_choice.RevealGeneral = function(self, choices)	
+		if askForShowGeneral(self, choices) == "yes" then return "yes" end
 
-		local anjiang = {}
-		for _, player in sgs.qlist(self.room:getAllPlayers()) do
-			if player:getGeneralName() == "anjiang" then table.insert(anjiang, player:getSeat()) end
-		end
-
-		if event == sgs.DamageInflicted then
-			local damage = data:toDamage()
-			for _, player in ipairs(players) do
-				if self:hasSkills(sgs.masochism_skill, player) and self:isEnemy(damage.from) then return "yes" end
-				if damage.damage > self.player:getHp() + self:getAllPeachNum() then return "yes" end
-			end
-		elseif event == sgs.CardEffected then
-			local effect = data:toCardEffect()
-			for _, player in ipairs(players) do
-				if self.room:isProhibited(effect.from, player, effect.card) and self:isEnemy(effect.from) then return "yes" end
-			end
-		end
-
-		if sgs.getValue(self.player) < 6 then return "no" end
 		for _, player in sgs.qlist(self.room:getOtherPlayers(self.player)) do
 			if self:isFriend(player) then return "yes" end
 		end
-		local vequips, defense = 0
-		if self.player:getWeapon() or self:hasHegSkills("yitian", players) then vequips = vequips + 1 end
-		if (self.player:getArmor() and self:evaluateArmor() > 0) or self:hasHegSkills("bazhen|yizhong", players) then
-			vequips = vequips + 2 defense = true end
-		if self.player:getDefensiveHorse() or self:hasHegSkills("feiying", players) then vequips = vequips + 1.5 defense = true end
-		if self.player:getOffensiveHorse() or self:hasHegSkills("mashu", players) then vequips = vequips + 0.5 end
-		if vequips < 2.5 or not defense then return "no" end
 
 		if sgs.ai_loyalty[self:getHegKingdom()][self.player:objectName()] == 160 then return "yes" end
 
@@ -109,7 +91,7 @@ if sgs.GetConfig("EnableHegemony", false) then
 		wei = {},
 		wu = {},
 		shu = {},
-		qun = {},
+		qun = {}
 	}
 	sgs.ai_explicit = {}
 

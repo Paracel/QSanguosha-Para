95c411c0d5c9c670505ab44fd28b6a134fee1993
Paracel <paracel_007@sina.com.cn>
2013-02-22 15:11:16 +0800

    choose 'discard' for XueHen with 3 or more Hp lost

--- lua/ai/bgm-ai.lua
+++ lua/ai/bgm-ai.lua
@@ -906,6 +906,10 @@ end
 sgs.ai_skill_choice.xuehen = function(self, choices)
 	local current = self.room:getCurrent();
 	if self:isEnemy(current) then
+		if self.player:getLostHp() >= 3 and current:getCardCount(true) >= 3 and not self:needKongcheng(current) 
+			and not (self:hasSkills(sgs.lose_equip_skill, current) and current:getHandcardNum() < self.player:getLostHp()) then
+			return "discard"
+		end
 		if self:hasSkills("jijiu|tuntian|beige", current) and self.player:getLostHp() >= 2 and current:getCardCount(true) >= 2 then return "discard" end
 	end
 	self:sort(self.enemies, "defenseSlash")

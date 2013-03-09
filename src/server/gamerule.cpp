d212f699c9a310ed54d55cdd5336cbc0c39760f8
Paracel <paracel_007@sina.com.cn>
2013-03-10 01:14:35 +0800

    typo

--- src/server/gamerule.cpp
+++ src/server/gamerule.cpp
@@ -1035,7 +1035,7 @@ bool BasaraMode::trigger(TriggerEvent event, Room *room, ServerPlayer *player, Q
                     log.arg2 = room->getTag(sp->objectName()).toStringList().at(1);
                 }
 
-                room->doNotify(player, QSanProtocol::S_COMMAND_LOG_SKILL, log.toJsonValue());
+                room->doNotify(sp, QSanProtocol::S_COMMAND_LOG_SKILL, log.toJsonValue());
                 sp->tag["roles"] = room->getTag(sp->objectName()).toStringList().join("+");
             }
         }

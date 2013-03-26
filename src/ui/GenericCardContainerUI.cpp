4eb7f254239a36ef4df85fa094d7bcad771bdeb5
Paracel <paracel_007@sina.com.cn>
2013-03-26 21:58:29 +0800

    fix HuaShen animation for the 2nd general

--- src/ui/GenericCardContainerUI.cpp
+++ src/ui/GenericCardContainerUI.cpp
@@ -662,14 +662,21 @@ void PlayerCardContainer::startHuaShen(QString generalName, QString skillName) {
     _m_huashenGeneralName = generalName;
     _m_huashenSkillName = skillName;
     Q_ASSERT(m_player->hasSkill("huashen"));
-    QPixmap pixmap = _getAvatarIcon(generalName);
-    if (pixmap.size() != _m_layout->m_avatarArea.size())
-        pixmap = pixmap.scaled(_m_layout->m_avatarArea.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+
+    bool second_zuoci = m_player && m_player->getGeneralName() != "zuoci" && m_player->getGeneral2Name() == "zuoci";
+    int avatarSize = second_zuoci ? _m_layout->m_smallAvatarSize :
+                                    (m_player->getGeneral2() ? _m_layout->m_primaryAvatarSize :
+                                                               _m_layout->m_avatarSize);
+    QPixmap pixmap = G_ROOM_SKIN.getGeneralPixmap(generalName, (QSanRoomSkin::GeneralIconSize)avatarSize);
+
+    QRect animRect = second_zuoci ? _m_layout->m_smallAvatarArea : _m_layout->m_avatarArea;
+    if (pixmap.size() != animRect.size())
+        pixmap = pixmap.scaled(animRect.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    if (second_zuoci)
+        pixmap = paintByMask(pixmap);
+
     stopHuaShen();
-    _m_huashenAnimation = G_ROOM_SKIN.createHuaShenAnimation(pixmap,
-                                                             _m_layout->m_avatarArea.topLeft(),
-                                                             _getAvatarParent(),
-                                                             _m_huashenItem);
+    _m_huashenAnimation = G_ROOM_SKIN.createHuaShenAnimation(pixmap, animRect.topLeft(), _getAvatarParent(), _m_huashenItem);
     _m_huashenAnimation->start();
     _paintPixmap(_m_extraSkillBg, _m_layout->m_extraSkillArea, QSanRoomSkin::S_SKIN_KEY_EXTRA_SKILL_BG, _getAvatarParent());
     _m_extraSkillBg->show();
@@ -778,6 +785,8 @@ void PlayerCardContainer::_adjustComponentZValues() {
 
     // layout
     if (!_startLaying()) return;
+    bool second_zuoci = m_player && m_player->getGeneralName() != "zuoci" && m_player->getGeneral2Name() == "zuoci";
+
     _layUnder(_m_floatingArea);
     _layUnder(_m_distanceItem);
     _layUnder(_m_votesItem);
@@ -810,8 +819,11 @@ void PlayerCardContainer::_adjustComponentZValues() {
     _layUnder(_m_smallAvatarArea);
     _layUnder(_m_avatarArea);
     _layUnder(_m_circleItem);
-    _layUnder(_m_smallAvatarIcon);
+    if (!second_zuoci)
+        _layUnder(_m_smallAvatarIcon);
     _layUnder(_m_huashenItem);
+    if (second_zuoci)
+        _layUnder(_m_smallAvatarIcon);
     _layUnder(_m_avatarIcon);    
 }
 

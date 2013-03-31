42a8747a2ec9b068d8cd7b95dc18572a9cd607f6
Paracel <paracel_007@sina.com.cn>
2013-04-01 07:41:33 +0800

    will you believe the result of YJCM 2013 today?

--- lua/config.lua
+++ lua/config.lua
@@ -2,7 +2,7 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "20130301",
+	version = "20130401",
 	version_name = "Alpha",
 	mod_name = "Para",
 	kingdoms = { "wei", "shu", "wu", "qun", "god" },

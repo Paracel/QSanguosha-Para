130042548b32f5638ba68986472eb0650988824a
Paracel <paracel_007@sina.com.cn>
2013-04-22 16:43:11 +0800

    minor adjustment on HuaShen AI by Mystery333

--- lua/ai/mountain-ai.lua
+++ lua/ai/mountain-ai.lua
@@ -1097,11 +1097,11 @@ function sgs.ai_skill_choice.huashen(self, choices)
 			end
 		end
 
-		if (self:getAllPeachNum() > 0 or not self:isWeak()) then
+		if self:getAllPeachNum() > 0 or self.player:getHp() > 1 or not self:isWeak() then
 			if str:matchOne("guixin") and self.room:alivePlayerCount() > 3 then return "guixin" end
 			if str:matchOne("yiji") then return "yiji" end
 			for _, askill in ipairs(("fankui|jieming|neoganglie|vsganglie|ganglie|enyuan|fangzhu|nosenyuan|langgu"):split("|")) do
-				if str:matchOne(askill) and (self.player:getHp() > 1 or self:getAllPeachNum() > 0) then return askill end
+				if str:matchOne(askill) then return askill end
 			end
 		end
 

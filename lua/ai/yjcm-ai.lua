a3d27026fda0b21df5b197a9af849aa54ba6c59b
Paracel <paracel_007@sina.com.cn>
2013-05-09 19:54:29 +0800

    adjustment on QuanJi AI

--- lua/ai/yjcm-ai.lua
+++ lua/ai/yjcm-ai.lua
@@ -801,7 +801,17 @@ sgs.ai_skill_invoke.shangshi = function(self, data)
 	return true
 end
 
-sgs.ai_skill_invoke.quanji = true
+sgs.ai_skill_invoke.quanji = function(self, data)
+	local current = self.room:getCurrent()
+	local juece_effect = (current and current:isAlive() and current:getPhase() ~= sgs.Player_NotActive and self:isEnemy(current) and current:hasSkill("juece"))
+	local manjuan_effect = (self.player:hasSkill("manjuan") and self.player:getPhase() == sgs.Player_NotActive)
+	if self.player:isKongcheng() then
+		if manjuan_effect or juece_effect then return false end
+	elseif self.player:getHandcardNum() == 1 then
+		if manjuan_effect and juece_effect then return false end
+	end
+	return true
+end
 
 sgs.ai_skill_discard.quanji = function(self)
 	local to_discard = {}

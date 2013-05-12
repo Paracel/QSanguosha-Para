a3d4e01f3efe138fbf42b1d517333c5860c873c0
Paracel <paracel_007@sina.com.cn>
2013-05-12 20:53:29 +0800

    fix the target filter in responding status

--- src/ui/roomscene.cpp
+++ src/ui/roomscene.cpp
@@ -1104,7 +1104,7 @@ void RoomScene::enableTargets(const Card *card) {
 
     if (card->targetFixed()
         || ((ClientInstance->getStatus() & Client::ClientStatusBasicMask) == Client::Responding
-            && ClientInstance->getStatus() != Client::RespondingUse)
+            && ClientInstance->getStatus() == Client::Responding)
         || ClientInstance->getStatus() == Client::AskForShowOrPindian) {
         foreach (PlayerCardContainer *item, item2player.keys()) {
             QGraphicsItem *animationTarget = item->getMouseClickReceiver();
@@ -2024,7 +2024,7 @@ void RoomScene::useSelectedCard() {
     case Client::Responding: {
             const Card *card = dashboard->getSelected();
             if (card) {
-                if (ClientInstance->getStatus() != Client::RespondingUse) {
+                if (ClientInstance->getStatus() == Client::Responding) {
                     Q_ASSERT(selected_targets.isEmpty());
                     selected_targets.clear();
                 }

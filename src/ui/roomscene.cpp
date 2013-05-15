e56bc7026bd43e92957e39db171b99541b8105b4
Paracel <paracel_007@sina.com.cn>
2013-05-15 13:09:14 +0800

    fix a bug of filtering targets at the client side

--- src/ui/roomscene.cpp
+++ src/ui/roomscene.cpp
@@ -1105,7 +1105,7 @@ void RoomScene::enableTargets(const Card *card) {
 
     if (card->targetFixed()
         || ((ClientInstance->getStatus() & Client::ClientStatusBasicMask) == Client::Responding
-            && ClientInstance->getStatus() == Client::Responding)
+            && ClientInstance->getStatus() != Client::RespondingUse)
         || ClientInstance->getStatus() == Client::AskForShowOrPindian) {
         foreach (PlayerCardContainer *item, item2player.keys()) {
             QGraphicsItem *animationTarget = item->getMouseClickReceiver();

4fbc71252502e0b03140bb45e3f1f6fe49a24eeb
Paracel <paracel_007@sina.com.cn>
2013-06-18 20:38:33 +0800

    typo in the Lua interface of validateInResponse

--- lua/sgs_ex.lua
+++ lua/sgs_ex.lua
@@ -154,7 +154,7 @@ function sgs.CreateSkillCard(spec)
 	card.on_use = spec.on_use
 	card.on_effect = spec.on_effect
 	card.on_validate = spec.on_validate
-	card_on_validate_in_response = spec.on_validate_in_response
+	card.on_validate_in_response = spec.on_validate_in_response
 
 	return card
 end

6bfb79e97c63e19ee16efc0b44b1937423c09105
Paracel <paracel_007@sina.com.cn>
2013-06-18 21:04:44 +0800

    fix a bug of GuHuo AI by a simplified but inaccurate method

--- lua/ai/wind-ai.lua
+++ lua/ai/wind-ai.lua
@@ -747,6 +747,8 @@ function SmartAI:getGuhuoViewCard(class_name)
 		if class_name == "Peach" or (class_name == "Analeptic" and not sgs.GetConfig("BanPackages", ""):match("maneuvering")) or class_name == "Jink" then
 			index = #card_use
 		end
+		local card = sgs.Sanguosha:cloneCard(classname2objectname[class_name])
+		if self.player:isCardLimited(card, sgs.Card_MethodUse, true) then return end
 		return "@GuhuoCard=" .. card_use[index]:getEffectiveId() .. ":" .. classname2objectname[class_name]
 	end
 end

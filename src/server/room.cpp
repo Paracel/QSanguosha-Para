02012cc0cc598b2150e43fc2210a128af937c68e
Paracel <paracel_007@sina.com.cn>
2013-06-19 17:21:21 +0800

    retrieve the Nullification AI helper

--- src/server/room.cpp
+++ src/server/room.cpp
@@ -969,6 +969,15 @@ bool Room::verifyNullificationResponse(ServerPlayer *player, const Json::Value &
 }
 
 bool Room::askForNullification(const Card *trick, ServerPlayer *from, ServerPlayer *to, bool positive) {
+    _NullificationAiHelper aiHelper;
+    aiHelper.m_from = from;
+    aiHelper.m_to = to;
+    aiHelper.m_trick = trick;
+    return _askForNullification(trick, from, to, positive, aiHelper);
+}
+
+bool Room::_askForNullification(const Card *trick, ServerPlayer *from, ServerPlayer *to,
+                                bool positive, _NullificationAiHelper aiHelper) {
     while (isPaused()) {}
 
     _m_roomState.setCurrentCardUseReason(CardUseStruct::CARD_USE_REASON_RESPONSE_USE);
@@ -1018,7 +1027,7 @@ bool Room::askForNullification(const Card *trick, ServerPlayer *from, ServerPlay
         foreach (ServerPlayer *player, validAiPlayers) {
             AI *ai = player->getAI();
             if (ai == NULL) continue;
-            card = ai->askForNullification(trick, from, to, positive);
+            card = ai->askForNullification(aiHelper.m_trick, aiHelper.m_from, aiHelper.m_to, positive);
             if (card && player->isCardLimited(card, Card::MethodUse))
                 card = NULL;
             if (card != NULL) {
@@ -1034,7 +1043,7 @@ bool Room::askForNullification(const Card *trick, ServerPlayer *from, ServerPlay
     card = card->validateInResponse(repliedPlayer);
 
     if (card == NULL)
-        return askForNullification(trick, from, to, positive);
+        return _askForNullification(trick, from, to, positive, aiHelper);
 
     doAnimate(S_ANIMATE_NULLIFICATION, repliedPlayer->objectName(), to->objectName());
     useCard(CardUseStruct(card, repliedPlayer, QList<ServerPlayer *>()));
@@ -1057,7 +1066,7 @@ bool Room::askForNullification(const Card *trick, ServerPlayer *from, ServerPlay
     effect.card = card;
     effect.to = repliedPlayer;
     if (card->isCancelable(effect))
-        result = !askForNullification(card, repliedPlayer, to, !positive);
+        result = !_askForNullification(card, repliedPlayer, to, !positive, aiHelper);
     return result;
 }
 

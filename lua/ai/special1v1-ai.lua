3b504eb06d52146291363054f24134250d032b98
Paracel <paracel_007@sina.com.cn>
2013-07-02 20:32:24 +0800

    MouZhu AI by lrl026

--- lua/ai/special1v1-ai.lua
+++ lua/ai/special1v1-ai.lua
@@ -251,7 +251,132 @@ sgs.ai_skill_invoke.manyi = function(self, data)
 	return (dummy_use.card ~= nil)
 end
 
--- @@todo: Mouzhu AI
+local mouzhu_skill = {}
+mouzhu_skill.name = "mouzhu"
+table.insert(sgs.ai_skills, mouzhu_skill)
+mouzhu_skill.getTurnUseCard = function(self)
+	if self.player:hasUsed("MouzhuCard") then return end
+	return sgs.Card_Parse("@MouzhuCard=.")
+end
+
+sgs.ai_skill_use_func.MouzhuCard = function(card, use, self)
+	local canleiji
+	if self.player:hasSkill("leiji") and self:findLeijiTarget(self.player, 50) and self:hasSuit("spade", true) then
+		canleiji = true
+		self:sort(self.friends_noself, "handcard")
+		sgs.reverse(self.friends_noself)
+		for _, friend in ipairs(self.friend_noself) do
+			if not friend:isKongcheng() and friend:getHandcardNum() < self.player:getHandcardNum() + 2
+				and (self.player:getCardsNum("Jink") > 0 or (not friend:hasWeapon("qinggang_sword") and not self:isWeak() and self:hasEightDiagramEffect())) then
+				use.card = card
+				if use.to then use.to:append(friend) end
+				return
+			end
+		end
+	end
+
+	for _, enemy in ipairs(self.enemies) do
+		if enemy:getHandcardNum() > 0 and  (self:getDamagedEffects(self.player, enemy) or self:needToLoseHp(self.player, enemy)) then
+			use.card = card
+			if use.to then use.to:append(enemy) end
+			return
+		end
+	end
+
+	local first, second, third, fourth
+	local slash = self:getCard("Slash")
+	local slash_nosuit = sgs.Sanguosha:cloneCard("slash")
+	for _, enemy in ipairs(self.enemies) do
+		if enemy:getHandcardNum() >= self.player:getHandcardNum() + 2 then
+			first = enemy
+			break
+		elseif enemy:getHandcardNum() > 0 then
+			if not self:slashIsEffective(slash_nosuit, self.player, nil, enemy) and self:getCardsNum("Slash") > getCardsNum("Slash", enemy) and not second then
+				second = enemy
+			elseif not enemy:hasSkills("wushuang|mengjin|tieji")
+				and not ((enemy:hasSkill("roulin") or enemy:hasWeapon("double_sword")) and enemy:getGender() ~= self.player:getGender()) then
+
+				if enemy:getHandcardNum() == 1 and slash and not third and self.player:inMyAttackRange(enemy)
+					and (self:hasHeavySlashDamage(self.player, slash, enemy) or self:hasWeapon("guding_blade") and not self:needKongcheng(enemy))
+					and (not self:isWeak() or self:getCardsNum("Peach") + self:getCardsNum("Analeptic") > 0) then
+					third = enemy
+				elseif self:getCardsNum("Jink") > 0 and self:getCardsNum("Slash") > getCardsNum("Slash", enemy) and not fourth then
+					fourth = enemy
+				end
+			end
+		end
+	end
+
+	local target
+	if canleiji then
+		target = fourth and third or first or second
+	else
+		target = first or second or third or fourth
+	end
+	if target then
+		use.card = card
+		if use.to then use.to:append(target) end
+		return
+	end
+end
+
+sgs.ai_skill_cardask["@mouzhu-give"] = function(self, data)
+	local target = self.room:getCurrent()
+	local cards = sgs.QList2Table(self.player:getHandcards())
+	self:sortByKeepValue(cards)
+	if self:isFriend(target) then
+		if target:hasSkill("leiji") then
+			local jink, spade
+			for _, c in ipairs(cards) do
+				if isCard("Jink", c, target) then jink = c:getEffectiveId() end
+				if c:getSuit() == sgs.Card_Spade then spade = c:getEffectiveId() end
+			end
+			if self:hasSuit("spade", true, target) and jink then return jink
+			elseif not self:hasEightDiagramEffect(target) and jink then return jink
+			elseif spade or jink then return spade or jink
+			end
+		end
+	else
+		if target:hasSkill("leiji") then
+			for _, c in ipairs(cards) do
+				if not c:isKindOf("Peach") and not c:isKindOf("Jink") and c:getSuit() ~= sgs.Card_Spade then
+					return c:getEffectiveId()
+				end
+			end
+		end
+		for _, c in ipairs(cards) do
+			if not c:isKindOf("Peach") then return c:getEffectiveId() end
+		end
+	end
+	return cards[1]:getEffectiveId()
+end
+
+sgs.ai_skill_choice.mouzhu = function(self, choices)
+	local target = self.room:getCurrent()
+	local slash = sgs.Sanguosha:cloneCard("slash")
+	if target:hasSkill("leiji") then
+		if self:isFriend(target) then
+			if choices:match("slash") then return "slash" end
+		else
+			if choices:match("duel") then return "duel" end
+		end
+	end
+
+	if self:isFriend(target) then
+		if (target:hasSkill("leiji") or not self:slashIsEffective(slash, target)) and choices:match("slash") then return "slash" end
+		if self:getDamagedEffects(self.player, target) and getCardsNum("Slash", target) >= 1 and choices:match("duel") then return "duel" end
+	else
+		if target:hasSkill("leiji") and choices:match("duel") then return "duel" end
+		if self:getCardsNum("Slash") > getCardsNum("Slash", target) and choices:match("duel") then return "duel" end
+	end
+
+	if choices:match("slash") then return "slash" else return "duel" end
+end
+
+sgs.ai_use_priority.MouzhuCard = 5.5
+sgs.ai_card_intention.MouzhuCard = function(self, card, from, tos)
+	if not self.player:hasSkill("leiji") then sgs.updateIntention(from, tos[1], 30) end
+end
 
 sgs.ai_skill_invoke.yanhuo = function(self, data)
 	local opponent = self.player:getOtherPlayers(true):first()

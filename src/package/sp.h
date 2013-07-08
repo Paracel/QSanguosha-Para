73290443219902c5d72fe7721d169a0a5e4385e1
Paracel <paracel_007@sina.com.cn>
2013-07-08 22:06:50 +0800

    split SP package

--- src/package/sp.h
+++ src/package/sp.h
@@ -12,6 +12,27 @@ public:
     SPPackage();
 };
 
+class OLPackage: public Package {
+    Q_OBJECT
+
+public:
+    OLPackage();
+};
+
+class TaiwanSPPackage: public Package {
+    Q_OBJECT
+
+public:
+    TaiwanSPPackage();
+};
+
+class WangZheZhiZhanPackage: public Package {
+    Q_OBJECT
+
+public:
+    WangZheZhiZhanPackage();
+};
+
 class Yongsi: public TriggerSkill {
     Q_OBJECT
 

7bb8bfedc417cb9485cca10cbb8bdd2face810a3
Paracel <paracel_007@sina.com.cn>
2013-07-17 16:56:41 +0800

    add Zhang Bao

--- src/package/sp.h
+++ src/package/sp.h
@@ -128,6 +128,16 @@ public:
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
+class ZhoufuCard: public SkillCard {
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE ZhoufuCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
+};
+
 class SPCardPackage: public Package {
     Q_OBJECT
 

7db1adb7b3bfe4ef3d3b7954ae6bf093dc00be66
Paracel <paracel_007@sina.com.cn>
2013-07-20 14:02:46 +0800

    minor adjustment on sgs.getDefense (last 7 commits by lrl026)

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -230,10 +230,12 @@ function sgs.getDefense(player, gameProcess)
 	if player:hasSkills("tuntian+zaoxian") then defense = defense + player:getHandcardNum() * 0.4 end
 	if player:hasSkill("aocai") and player:getPhase() == sgs.Player_NotActive then defense = defense + 0.3 end
 	if attacker and not attacker:hasSkill("jueqing") then
-		local m = sgs.masochism_skill:split("|")
-		for _, masochism in ipairs(m) do
-			if player:hasSkill(masochism) then
-				defense = defense + 1
+		if not gameProcess and sgs.isGoodHp(player) then
+			local m = sgs.masochism_skill:split("|")
+			for _, masochism in ipairs(m) do
+				if player:hasSkill(masochism) then
+					defense = defense + 1
+				end
 			end
 		end
 		if player:hasSkill("jieming") then defense = defense + 3 end

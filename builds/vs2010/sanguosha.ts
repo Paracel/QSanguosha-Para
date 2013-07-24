daac04bead0b976551477ca3c5a50f4abca35a73
Paracel <paracel_007@sina.com.cn>
2013-07-24 22:31:11 +0800

    add new SP Lady Xiahou

--- builds/vs2010/sanguosha.ts
+++ builds/vs2010/sanguosha.ts
@@ -1315,7 +1315,7 @@
         <translation>重复技能: %1</translation>
     </message>
     <message>
-        <location filename="../../src/core/engine.cpp" line="657"/>
+        <location filename="../../src/core/engine.cpp" line="661"/>
         <source>%1 [Scenario mode]</source>
         <translation>%1 【剧情模式】</translation>
     </message>

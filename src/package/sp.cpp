39c94a18d18856c84b79b1b5e4b0172370f9ad7b
Paracel <paracel_007@sina.com.cn>
2013-07-30 17:38:15 +0800

    fix a bug of XiahouShi

--- src/package/sp.cpp
+++ src/package/sp.cpp
@@ -1203,7 +1203,7 @@ private:
         if (!general) return QStringList();
         QStringList skill_list;
         foreach (const Skill *skill, general->getSkillList()) {
-            if (!skill->isLordSkill() && skill->getFrequency() != Skill::Wake)
+            if (skill->isVisible() && !skill->isLordSkill() && skill->getFrequency() != Skill::Wake)
                 skill_list.append(skill->objectName());
         }
         return skill_list;

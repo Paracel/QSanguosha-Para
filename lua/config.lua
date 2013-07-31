ffcfb2f63e68c6a037ff8d84fdc39abed3c4bf5f
Paracel <paracel_007@sina.com.cn>
2013-08-01 00:08:51 +0800

    August...

--- lua/config.lua
+++ lua/config.lua
@@ -2,7 +2,7 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "20130701",
+	version = "20130801",
 	version_name = "V2",
 	mod_name = "Para",
 	kingdoms = { "wei", "shu", "wu", "qun", "god" },

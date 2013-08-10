eb8cb5a4ab903044d3022bc1ee855f4e9db843e4
Paracel <paracel_007@sina.com.cn>
2013-08-11 00:03:03 +0800

    add slash parameter for Player::canSlashWithoutCrossbow

--- src/package/standard-cards.cpp
+++ src/package/standard-cards.cpp
@@ -28,7 +28,7 @@ bool Slash::IsAvailable(const Player *player, const Card *slash) {
     if (player->isCardLimited(THIS_SLASH, Card::MethodUse))
        return false;
 
-    if (player->hasWeapon("crossbow") || player->canSlashWithoutCrossbow())
+    if (player->hasWeapon("crossbow") || player->canSlashWithoutCrossbow(THIS_SLASH))
         return true;
     int used = player->getSlashCount();
     int valid = 1 + Sanguosha->correctCardTarget(TargetModSkill::Residue, player, newslash);

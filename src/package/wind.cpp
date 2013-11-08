f28e2800e30dafdcee7822cc97d3483c45f53c17
Paracel <paracel_007@sina.com.cn>
2013-11-08 15:47:45 +0800

    adjust FenJi

--- src/package/wind.cpp
+++ src/package/wind.cpp
@@ -507,7 +507,7 @@ public:
 
     virtual bool trigger(TriggerEvent, Room *room, ServerPlayer *player, QVariant &data) const{
         CardsMoveOneTimeStruct move = data.value<CardsMoveOneTimeStruct>();
-        if (move.from && move.from->isAlive() && move.from_places.contains(Player::PlaceHand)
+        if (player->getHp() > 0 && move.from && move.from->isAlive() && move.from_places.contains(Player::PlaceHand)
             && ((move.reason.m_reason == CardMoveReason::S_REASON_DISMANTLE
                  && move.reason.m_playerId != move.reason.m_targetId)
                 || (move.to && move.to != move.from && move.to_place == Player::PlaceHand))) {

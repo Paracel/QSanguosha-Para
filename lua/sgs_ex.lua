fa7462b17b9eec550d6c75629a424f18cafe65fd
Paracel <paracel_007@sina.com.cn>
2013-12-30 20:15:50 +0800

    typos in sgs_ex.lua fixed by Fsu0413

--- lua/sgs_ex.lua
+++ lua/sgs_ex.lua
@@ -74,10 +74,14 @@ end
 
 function sgs.CreateMaxCardsSkill(spec)
 	assert(type(spec.name) == "string")
-	assert(type(spec.extra_func) == "function")
+	assert(type(spec.extra_func) == "function" or type(spec.fixed_func) == "function")
 
 	local skill = sgs.LuaMaxCardsSkill(spec.name)
-	skill.extra_func = spec.extra_func
+	if spec.extra_func then
+		skill.extra_func = spec.extra_func
+	else
+		skill.fixed_func = spec.fixed_func
+	end
 
 	return skill
 end
@@ -486,7 +490,7 @@ function sgs.CreateOneCardViewAsSkill(spec)
 			local pat = spec.filter_pattern
 			if string.endsWith(pat, "!") then
 				if sgs.Self:isJilei(to_select) then return false end
-				pat = string.sub(pat, 1, -1)
+				pat = string.sub(pat, 1, -2)
 			end
 			return sgs.Sanguosha:matchExpPattern(pat, sgs.Self, to_select)
 		end

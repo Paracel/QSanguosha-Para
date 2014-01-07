baa6bd07d1a980d740cc9261a0cb81a7a9599d77
Paracel <paracel_007@sina.com.cn>
2014-01-07 22:42:54 +0800

    use Analeptic when overflow > 0

--- lua/ai/maneuvering-ai.lua
+++ lua/ai/maneuvering-ai.lua
@@ -188,7 +188,7 @@ function SmartAI:shouldUseAnaleptic(target, slash)
 	end
 
 	if getKnownCard(target, self.player, "Jink", true, "he") >= 1 and not (self:getOverflow() > 0 and self:getCardsNum("Analeptic") > 1) then return false end
-	return self:getCardsNum("Analeptic") > 1 or getCardsNum("Jink", target, self.player) < 1 or sgs.card_lack[target:objectName()]["Jink"] == 1
+	return self:getCardsNum("Analeptic") > 1 or getCardsNum("Jink", target, self.player) < 1 or sgs.card_lack[target:objectName()]["Jink"] == 1 or self:getOverflow() > 0
 end
 
 sgs.dynamic_value.benefit.Analeptic = true

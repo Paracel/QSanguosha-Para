5d87b19a7c3215882f86cc785f3ae3971d6dfcde
Paracel <paracel_007@sina.com.cn>
2014-02-09 21:18:42 +0800

    Jink for Analeptic-Slash

--- lua/ai/standard_cards-ai.lua
+++ lua/ai/standard_cards-ai.lua
@@ -851,6 +851,10 @@ sgs.ai_skill_cardask["slash-jink"] = function(self, data, pattern, target)
 				return "."
 			end
 		end
+		if self.player:getHp() > 1 and getKnownCard(target, self.player, "Slash") >= 1 and getKnownCard(target, self.player, "Analeptic") >= 1 and self:getCardsNum("Jink") == 1
+			and (target:getPhase() < sgs.Player_Play or (self:slashIsAvailable(player) and player:canSlash(self.player))) then
+			return "."
+		end
 		if not (target:hasSkill("nosqianxi") and target:distanceTo(self.player) == 1) then
 			if target:hasWeapon("axe") then
 				if target:hasSkills(sgs.lose_equip_skill) and target:getEquips():length() > 1 and target:getCards("he"):length() > 2 then return "." end

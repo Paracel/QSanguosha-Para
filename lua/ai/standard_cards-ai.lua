cba9ca1d4350779575f1bae7455362e3e1ef1803
Paracel <paracel_007@sina.com.cn>
2014-03-20 14:03:20 +0800

    minor adjustment on Indulgence AI by lrl026

--- lua/ai/standard_cards-ai.lua
+++ lua/ai/standard_cards-ai.lua
@@ -2610,6 +2610,8 @@ function SmartAI:useCardIndulgence(card, use)
 		if enemy:hasSkills("tuxi|nostuxi|noszhenlie|guanxing|qinyin|zongshi|tiandu") then value = value - 3 end
 		if self:needBear(enemy) then value = value - 20 end
 		if not sgs.isGoodTarget(enemy, self.enemies, self) then value = value - 1 end
+		if getKnownCard(enemy, self.player, "Dismantlement", true) > 0 then value = value + 2 end
+		value = value + (self.room:alivePlayerCount() - self:playerGetRound(enemy)) / 2
 		return value
 	end
 

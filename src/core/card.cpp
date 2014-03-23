4c1e7e4d5e9248658a2d7d71dcb947c98da6c999
Paracel <paracel_007@sina.com.cn>
2014-03-23 13:49:49 +0800

    typo in Card::use (last 2 commits by Slob)

--- src/core/card.cpp
+++ src/core/card.cpp
@@ -629,7 +629,7 @@ void Card::use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets)
             effect.card = this;
             effect.from = source;
             effect.to = target;
-            effect.multiple = false;
+            effect.multiple = true;
 
             room->cardEffect(effect);
         }

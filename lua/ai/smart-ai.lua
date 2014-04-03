d3d26df44b4341347a3c45c1d3d51ed6ce366732
Paracel <paracel_007@sina.com.cn>
2014-04-03 15:54:12 +0800

    April

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -9,7 +9,7 @@ math.randomseed(tostring(os.time()):reverse():sub(1, 6))
 -- SmartAI is the base class for all other specialized AI classes
 SmartAI = (require "middleclass").class("SmartAI")
 
-version = "QSanguosha AI 20140301 (V1.414 Alpha)"
+version = "QSanguosha AI 20140401 (V1.4142 Alpha)"
 --- this function is only function that exposed to the host program
 --- and it clones an AI instance by general name
 -- @param player The ServerPlayer object that want to create the AI object
--- lua/config.lua
+++ lua/config.lua
@@ -2,7 +2,7 @@
 -- and it is a little different from config.ini
 
 config = {
-	version = "20140301",
+	version = "20140401",
 	version_name = "V2",
 	mod_name = "Para",
 	kingdoms = { "wei", "shu", "wu", "qun", "god" },

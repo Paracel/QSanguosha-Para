02949751dac9e4f83b3417f5af3803f510938103
Paracel <paracel_007@sina.com.cn>
2014-06-26 23:13:31 +0800

    typo in WoodenOx::unInstall (last 2 commits by Slob)

--- src/package/standard-cards.cpp
+++ src/package/standard-cards.cpp
@@ -1351,6 +1351,7 @@ WoodenOx::WoodenOx(Suit suit, int number)
 
 void WoodenOx::onUninstall(ServerPlayer *player) const{
     player->getRoom()->addPlayerHistory(player, "WoodenOxCard", 0);
+    Treasure::onUnistall(player);
 }
 
 StandardCardPackage::StandardCardPackage()
